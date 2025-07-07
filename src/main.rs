b5aed0bab5c08811832ed4efef9a8e88bc7b081e
bghullu <ghullu.bijay@gmail.com>
2025-07-07 15:48:17 +0545

    Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
+     
+        println!("{}", fib(10));
 
-}
+    }
+
+    fn fib (num: u32) -> u32 {
+        let mut first = 0;
+        let mut second = 1;
+        if num ==0{
+            return first;
+
+        }
+
+        if num == 1 {
+            return second;
+        }
+
+        for _ in 0..(num - 2){
+            let temp = second;
+            second = second +first;
+            first = temp;
+        }
+        return second;
+
+
+    }
+
+
