cea92af91baf0e5787742045292ba90c956351a7
bghullu <ghullu.bijay@gmail.com>
2025-07-10 19:34:07 +0545

    Generics

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,81 @@
 
-// -------------------- Method & Associate Functions ------------------
+// -------------------- Generics ------------------
 
 
-struct Rectangle {
-    width: u32,
-    height: u32,
+fn Sum<T: std::ops::Add<Output =T>>(a:T,b:T) ->T{
+    a + b
+}
+struct Point<T> {
+    x:  T,
+    y: T,
 }
 
-impl Rectangle{
-    fn area(self) -> u32 {
-        self.width * self.height
+struct Dif<T,U>{
+    x:T ,
+    y: U,
+}
+
+struct Val<T>{
+    val: T,
+}
+
+impl<T> Val<T>{
+    fn value(&self)-> &T{
+        &self.val
     }
 }
 
-struct TrafficLight{
-    color: String
+struct Poin<T,U>{
+    x: T,
+    y: U,
 }
-
-enum TrafficLightColor{
-    Red,
-    Yellow,
-    Green,
-}
-impl TrafficLightColor{
-    fn color(&self) -> &str {
-        match self {
-            Self::Yellow => "Yellow",
-            Self::Red => "Red",
-            Self::Green => "Green",
+impl<T,U> Poin<T,U>{
+    fn mixup<V,W>(self, other: Poin<V,W>) -> Poin<T,W>{
+        Poin {
+             x: self.x,
+             y: other.y,
         }
     }
 }
 
-
-
-impl TrafficLight{
-
-    pub fn new() -> Self {
-        Self { 
-            color: String::from("Red")
-        }
-    }
-
-    pub fn get_state(&self) -> &str{
-        &self.color
-    }
-    
-    pub fn show_state(self: &Self) { // &self is similar. to self: &self
-        println!("The current state is {}", self.color)
-    } 
-
-    pub fn change_state(&mut self){
-        self.color = "green".to_string()
-    }
+struct Array<T, const N: usize>{
+    data: [T;N]
 }
 fn main (){
-    let rect1 = Rectangle{width: 10, height: 10};
-    assert_eq!(rect1.area(),100);
+    let integer:Point<i32>= Point{x:5, y:10};
+    let float : Point<f64>= Point{x:2.0 , y:1.0};
+    let diff: Dif<i32,String> = Dif{x:10, y: "hello".to_string()};
 
+    let x =Val{val:3.0};
+    let y=Val{val:"hello".to_string()};
+    println!("{},{} ", x.value(), y.value());
 
-    let light: TrafficLight = TrafficLight::new();
-    assert_eq!(light.get_state(), "Red");
+    let p1: Poin<i32, i32> = Poin{x:5, y:10};
+    let p2: Poin<&str, char> = Poin{x:"Hello", y: 'c'};
+    let p3 = p1.mixup(p2);
 
-    let c  : TrafficLightColor = TrafficLightColor::Yellow;
-    assert_eq!(c.color(), "Yellow");
+    assert_eq!(p3.x, 5);
+    assert_eq!(p3.y,'c');
 
-    println!("Success!")
+    let arrays: [Array<i32,3>;3]=[
+        Array{
+            data: [1,2,3],
+        },
+        Array{
+            data:[1,2,3],
+        },
+        Array{
+            data:[4,5,6]
+        },
+    ];
 
+    let floats: [Array<f64,3>;2]=[
+        Array{
+            data:[1.0,2.0,3.0]
+        },
+        Array{
+            data: [4.0,5.0,6.0]
+        },
+    ];
 }
+
