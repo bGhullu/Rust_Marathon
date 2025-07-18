9f72cf7eeb8f72e03a89c66e33e53958aa1cf09e
bghullu <ghullu.bijay@gmail.com>
2025-07-19 01:03:26 +0545

    Delimiter

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,49 @@
-// #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
-
-// pub struct StrSplit<'a>{
-//     remainder: &'a str,
-//     delimiter: &'a str,
-// }
-
-// impl StrSplit<'_> {
-//     pub fn new(haystack: &str, delimiter: &str) -> Self {
-//         Self{
-//             remainder: haystack,
-//             delimiter,
-//         }
-//     }
-// }
-
-// impl<'a> Iterator for StrSplit<'a>{
-//     type Item = &'a str;
-//     fn next(&mut self ) -> Option< Self::Item>{
-//        if let Some(next_delim) = self.remainder.find(self.delimiter){   
-//             let until_delimiter = &self.remainder[..next_delim];
-//             self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
-//             Some(until_delimiter)
-//        } else if self.remainder.is_empty(){
-//             None
-//        }else{
-//         let rest = self.remainder;
-//         self.remainder = &[];
-//         Some(rest)
-//        }
-//     }
+//! This crate provides a custom iterator for splitting strings by a delimiter.
+//!
+//! It includes the `StrSplit` struct, which implements the `Iterator` trait.
+#![warn(missing_debug_implementations, rust_2018_idioms,missing_docs)]
 
 
-// }
+/// An iterator that splits a string by a delimiter.
+#[derive(Debug)]
+pub struct StrSplit<'a>{
+    remainder: &'a str,
+    delimiter: &'a str,
+}
+// implement struct
+impl<'a> StrSplit<'a>{
+        /// Creates a new `StrSplit` iterator over `haystack`, splitting by `delimiter`.
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self{
+        Self { 
+            remainder: haystack,
+            delimiter,
+        }
+    }
+}
+
+impl<'a> Iterator for StrSplit<'a>{
+    type Item= &'a str;
+
+    /// Returns the next substring until the delimiter, or `None` if done.
+    fn next(&mut self)-> Option<Self::Item>{
+        if let Some(next_delim)= self.remainder.find(self.delimiter){
+            let until_delimeter = &self.remainder[..next_delim];
+            self.remainder= &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimeter)
+            
+        }else if self.remainder.is_empty(){
+            None
+        }else {
+            let rest = self.remainder;
+            self.remainder= "";
+            Some(rest)
+        }
+    }
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack,  " ");
+    assert!(letters.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
+}
