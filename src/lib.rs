4b4c4709f110d8ca7208d798ee3516e095507bc9
bghullu <ghullu.bijay@gmail.com>
2025-07-20 15:06:23 +0545

    Delimiter

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@
 //! It includes the `StrSplit` struct, which implements the `Iterator` trait.
 #![warn(missing_debug_implementations, rust_2018_idioms,missing_docs)]
 
+use std::ops::RemAssign;
+
 
 /// An iterator that splits a string by a delimiter.
 #[derive(Debug)]
 pub struct StrSplit<'a>{
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str,
 }
 // implement struct
@@ -15,7 +17,7 @@ impl<'a> StrSplit<'a>{
         /// Creates a new `StrSplit` iterator over `haystack`, splitting by `delimiter`.
     pub fn new(haystack: &'a str, delimiter: &'a str) -> Self{
         Self { 
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter,
         }
     }
@@ -26,20 +28,30 @@ impl<'a> Iterator for StrSplit<'a>{
 
     /// Returns the next substring until the delimiter, or `None` if done.
     fn next(&mut self)-> Option<Self::Item>{
-        println!("Calling .next() on StrSplit");
-        if let Some(next_delim)= self.remainder.find(self.delimiter){
-            let until_delimeter = &self.remainder[..next_delim];
-            self.remainder= &self.remainder[(next_delim + self.delimiter.len())..];
-            Some(until_delimeter)
+
+        let ref mut remainder= self.remainder?; // let remainder = &mut self.remainder;
+            if let Some(next_delim)= remainder.find(self.delimiter){
+                let until_delimeter = &.remainder[..next_delim];
+                *remainder= &remainder[(next_delim + self.delimiter.len())..];
+                Some(until_delimeter)
             
-        }else if self.remainder.is_empty(){
-            None
-        }else {
-            let rest = self.remainder;
-            self.remainder= "";
-            Some(rest)
-        }
-    }
+            }else {
+                self.remainder.take()
+            } 
+    }    
+}   
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+#[test]
+fn it_works0() {
+    let haystack = "a b c d ";
+    let letters: Vec<_> = StrSplit::new(haystack,  " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
 }
 
 #[test]
@@ -47,4 +59,5 @@ fn it_works() {
     let haystack = "a b c d e";
     let letters: Vec<_> = StrSplit::new(haystack,  " ").collect();
     assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
+}
 }
