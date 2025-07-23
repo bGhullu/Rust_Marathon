a400447abede2a7f5e789442f19ab9593341b11f
bghullu <ghullu.bijay@gmail.com>
2025-07-23 17:39:44 +0545

    spawn

--- src/simultaneous.rs
+++ src/simultaneous.rs
@@ -1,15 +1,15 @@
-fn main () {
+pub fn simultaneous () {
     let runtime = tokio::runtime::Runtime::new();
     runtime.block_on(async{
         let mut accetp = tokio::net::TcpListener::bind("0.0.0.0:8080");
         while let Ok(stream) = accept.wait {
-            tokio::spwan(handle_conection(stream));
+            tokio::spawn(handle_conection(stream));
         }
     });
 }
 
 async fn handle_conection(_: TcpSteam){
-    tokio::spwan(async{
-        
+    tokio::spawn(async{
+
     })
 }
