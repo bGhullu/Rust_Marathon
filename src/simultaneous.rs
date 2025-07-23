4c4a96723413cb1acda213f260d67bf81469164c
bghullu <ghullu.bijay@gmail.com>
2025-07-23 17:38:10 +0545

    spawn

--- src/simultaneous.rs
+++ src/simultaneous.rs
@@ -2,20 +2,14 @@ fn main () {
     let runtime = tokio::runtime::Runtime::new();
     runtime.block_on(async{
         let mut accetp = tokio::net::TcpListener::bind("0.0.0.0:8080");
-        let mut connection = futures::future::FuturesUnordered::ned();
-        loop {
-            select! {
-                stream <- (&mutaccept).await =>. {
-                    connections.push(handle_conection(stream));
-                }
-                _ <- (&mut connections).await => {}
-
-                
-            }
+        while let Ok(stream) = accept.wait {
+            tokio::spwan(handle_conection(stream));
         }
     });
 }
 
 async fn handle_conection(_: TcpSteam){
-    todo! ()
+    tokio::spwan(async{
+        
+    })
 }
